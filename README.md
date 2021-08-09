fb50b0e61c9c2d02b45b1e70d1309062da19717f
Muhan Li <github@umm.one>
2021-08-10 03:12:59 +0800

    rename parameters

--- README.md
+++ README.md
@@ -2,32 +2,66 @@
 
 A web clock to display local time.
 
-[clock.github.rank.technology](https://clock.github.rank.technology/?sp=hmsColor)
+[clock.github.rank.technology](https://clock.github.rank.technology/?special=hmsColor)
 
 ## Usage
 
 <details>
-  <summary>Parameters</summary>
+  <summary>Customization Parameters</summary>
+  <table>
+    <thead>
+      <tr>
+        <th width="200">Key Name</th>
+        <th width="300">Description</th>
+        <th width="150">Default Value</th>
+      </tr>
+    </thead>
+    <tbody align="center">
+      <tr>
+        <td><strong>text</strong></td>
+        <td>hexadecimal color codes</td>
+        <td>f8f9fa</td>
+      </tr>
+      <tr>
+        <td><strong>background</strong></td>
+        <td>hexadecimal color codes</td>
+        <td>212529</td>
+      </tr>
+      <tr>
+        <td><strong>interval</strong></td>
+        <td>milliseconds</td>
+        <td>1000</td>
+      </tr>
+      <tr>
+        <td><strong>rotate</strong></td>
+        <td>degrees</td>
+        <td>0</td>
+      </tr>
+      <tr>
+        <td><strong>special</strong></td>
+        <td>special effects</td>
+        <td>none</td>
+      </tr>
+    </tbody>
+  </table>
 
-  ```
-  https://clock.github.rank.technology/?fg=f8f9fa&bg=212529&int=1000 (default)
-  
-  fg:  Font color Hex cod
-  bg:  Background color Hex code
-  int: Interval (milliseconds)
-  rot: Rotation (degrees) [-90, 0, 90, 180, 270]
-  ```
-</details>
-
-<details>
-  <summary>Special Effects</summary>
-
-  ```
-  https://clock.github.rank.technology/?sp=none (default)
-  
-  sp:  Special Effects
-       hmsColor: set the background color to #hhmmss
-  ```
+  <details>
+    <summary>Special Effects</summary>
+    <table>
+      <thead>
+        <tr>
+          <th width="200">Value Name</th>
+          <th width="450">Description</th>
+        </tr>
+      </thead>
+      <tbody align="center">
+        <tr>
+          <td><strong>hmsColor</strong></td>
+          <td>set the background color to #hhmmss</td>
+        </tr>
+      </tbody>
+    </table>
+  </details>
 </details>
 
 ## Maintainers
--- assets/js/script.js
+++ assets/js/script.js
@@ -40,17 +40,43 @@ function update_time(week_id, date_id, time_id) {
   document.getElementById(time_id.id).innerHTML = time_text;
 }
 
+function rotate_page(degrees) {
+  let rotation = "rotate(" + degrees + "deg)";
+
+  page.style.setProperty("transform", rotation);
+  page.style.setProperty("-webkit-transform", rotation);
+
+  if (degrees == "90") {
+    page.style.setProperty("transform-origin", "bottom left");
+    page.style.setProperty("-webkit-transform-origin", "bottom left");
+    page.style.setProperty("position", "absolute");
+    page.style.setProperty("top", "-100vw");
+    page.style.setProperty("height", "100vw");
+    page.style.setProperty("width", "100vh");
+  }
+
+  if (degrees == "-90" || degrees == "270") {
+    page.style.setProperty("transform-origin", "bottom right");
+    page.style.setProperty("-webkit-transform-origin", "bottom right");
+    page.style.setProperty("position", "absolute");
+    page.style.setProperty("top", "-100vw");
+    page.style.setProperty("left", "calc(100vw - 100vh)");
+    page.style.setProperty("height", "100vw");
+    page.style.setProperty("width", "100vh");
+  }
+}
+
 window.addEventListener("load", function () {
   let page = document.getElementById("wrapper");
 
-  let fg_color = get_query_variable("fg");
+  let fg_color = get_query_variable("text");
   if (fg_color != null) {
     page.style.setProperty("color", "#" + fg_color);
   } else {
     page.style.setProperty("color", "#f8f9fa");
   }
 
-  let bg_color = get_query_variable("bg");
+  let bg_color = get_query_variable("background");
   if (bg_color != null) {
     page.style.setProperty("background-color", "#" + bg_color);
   } else {
@@ -59,48 +85,26 @@ window.addEventListener("load", function () {
 
   update_time(week, date, time);
 
-  let interval = get_query_variable("int");
+  let interval = get_query_variable("interval");
   if (!isNaN(interval) && !isNaN(parseInt(interval))) {
     setInterval("update_time(week, date, time)", parseInt(interval));
   } else {
     setInterval("update_time(week, date, time)", 1000);
   }
 
-  let rotation = get_query_variable("rot");
-  if (["180", "90", "-90", "270"].includes(rotation)) {
-    let rot = "rotate(" + rotation + "deg)";
-
-    page.style.setProperty("transform", rot);
-    page.style.setProperty("-webkit-transform", rot);
-
-    if (rotation == "90") {
-      page.style.setProperty("transform-origin", "bottom left");
-      page.style.setProperty("-webkit-transform-origin", "bottom left");
-      page.style.setProperty("position", "absolute");
-      page.style.setProperty("top", "-100vw");
-      page.style.setProperty("height", "100vw");
-      page.style.setProperty("width", "100vh");
-    }
-
-    if (rotation == "-90" || rotation == "270") {
-      page.style.setProperty("transform-origin", "bottom right");
-      page.style.setProperty("-webkit-transform-origin", "bottom right");
-      page.style.setProperty("position", "absolute");
-      page.style.setProperty("top", "-100vw");
-      page.style.setProperty("left", "calc(100vw - 100vh)");
-      page.style.setProperty("height", "100vw");
-      page.style.setProperty("width", "100vh");
-    }
+  let degrees = get_query_variable("rotation");
+  if (["180", "90", "-90", "270"].includes(degrees)) {
+    rotate_page(degrees);
   }
 
-  let special = get_query_variable("sp");
+  let special = get_query_variable("special");
   if (special == "hmsColor") {
-    sp_hms_color(page);
-    setInterval("sp_hms_color(page)", 1000);
+    special_hmsColor(page);
+    setInterval("special_hmsColor(page)", 1000);
   }
 });
 
-function sp_hms_color(element) {
+function special_hmsColor(element) {
   let date = new Date();
 
   let h = ("0" + date.getHours()).slice(-2);
